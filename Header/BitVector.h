89218b9a2e8cf4414ad361101dfbf354e074b7d6
ArmenParanyan <80101033+ArmParanyan@users.noreply.github.com>
2023-01-17 22:42:02 +0400

    Add files via upload

---
+++ Header/BitVector.h
@@ -0,0 +1,33 @@
+#pragma once 
+
+#include <iostream>
+#include <vector>
+
+class BitVector
+{
+public:
+    BitVector();
+    BitVector(const BitVector& rhs);
+    BitVector(BitVector&&);
+    ~BitVector();
+
+public:
+    int& operator[](const int ind);
+
+public:
+    friend std::ostream& operator<<(std::ostream& os, const BitVector& vec);
+
+public:
+    const static size_t bitCount = 64;
+    void set(size_t ind, bool val);
+    bool get(int ind);
+    int size() const;
+
+private:
+    void resize(int);
+
+private:
+    int* m_arr;
+    size_t m_size;
+
+};
